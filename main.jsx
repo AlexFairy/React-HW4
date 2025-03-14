4129795b786a5c53d4240737a8b3b19d9a711a27
AlexFairy <AlexM121687@gmail.com>
2025-03-14 15:39:54 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
 {/* */}
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import the new createRoot API
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// Create a root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
